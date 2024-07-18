a5cd2224babe2ef9578c6b51814ac8e5fc3b098a
Deiver Julio <deiver-jc@live.com>
2024-07-17 20:42:29 -0500

    publish restify auth files

--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,18 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::restifyAuth();
+
+Route::post('login', \App\Http\Controllers\Restify\Auth\LoginController::class)
+    ->middleware('throttle:6,1')
+    ->name('restify.login');
+Route::post('register', \App\Http\Controllers\Restify\Auth\RegisterController::class)
+    ->name('restify.register');
+Route::post('forgotPassword', \App\Http\Controllers\Restify\Auth\ForgotPasswordController::class)
+    ->middleware('throttle:6,1')
+    ->name('restify.forgotPassword');
+Route::post('resetPassword', \App\Http\Controllers\Restify\Auth\ResetPasswordController::class)
+    ->middleware('throttle:6,1')
+    ->name('restify.resetPassword');
+Route::post('verify/{id}/{hash}', \App\Http\Controllers\Restify\Auth\VerifyController::class)
+    ->middleware('throttle:6,1')
+    ->name('restify.verify');
