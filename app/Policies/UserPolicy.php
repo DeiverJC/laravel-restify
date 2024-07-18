1f79cc12a3e9ddd99f6c56a68e5e5398b3e7abb3
Deiver Julio <deiver-jc@live.com>
2024-07-17 21:21:19 -0500

    update UserPolicy

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -31,7 +31,7 @@ class UserPolicy
 
     public function update(User $user, User $model): bool
     {
-        return false;
+        return $user->id === $model->id;
     }
 
     public function updateBulk(User $user, User $model): bool
